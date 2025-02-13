6c25b3f8c84015c086e621d75f5981795f4ed31c
Vincent S. <vincent.silvestri@hotmail.fr>
2025-02-13 12:44:19 +0100

    mise à jour models

--- models/hashtag.js
+++ models/hashtag.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const hashtagSchema = mongoose.Schema({
   hashtag: String,
+  tweet: { type: mongoose.Schema.Types.ObjectId, ref: "tweets" },
 });
 
 const Hashtag = mongoose.model("hashtags", hashtagSchema);
