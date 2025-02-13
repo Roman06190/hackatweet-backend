864dca2a3b2abf4e466a8ecf11220b509f4a3521
Vincent S. <vincent.silvestri@hotmail.fr>
2025-02-13 15:50:56 +0100

    route hashtag à supprimer

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var tweetRouter = require("./routes/tweet");
+var hashtagRouter = require("./routes/hashtag");
 
 var app = express();
 
@@ -24,5 +25,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/tweet", tweetRouter);
+app.use("/hashtag", hashtagRouter);
 
 module.exports = app;
