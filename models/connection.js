d0189f4421dbfe2100313e0e3823a6323f3fc9c9
Vincent S. <vincent.silvestri@hotmail.fr>
2025-02-13 10:17:13 +0100

    r

--- models/connection.js
+++ models/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://romandelpias:Roman200628@cluster0.osexk.mongodb.net/kackatweet";
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log("Database connected"))
-
   .catch((error) => console.error(error));
