e539a8161b50905ec17dc9d4dc9d38265a36548d
Roman <romandelpias@gmail.com>
2025-02-13 17:17:11 +0100

    route alltweet

---
+++ routes/tweet.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+const mongoose = require("mongoose");
+
+require("../models/connection");
+const Tweet = require("../models/tweet");
+
+router.post("/tweet", (req, res) => {
+  console.log("req:", req.body.id);
+  const newTweet = new Tweet({
+    content: req.body.tweet,
+    date: Date.now(),
+    author: req.body.id,
+    hashtag: req.body.id,
+  });
+  newTweet.save().then((data) => {
+    res.json({
+      result: true,
+      content: req.body.tweet,
+      author: req.body.id,
+    });
+  });
+});
+
+router.get("/alltweet", (req, res) => {
+  Tweet.find().then((data) => {
+    res.json({ allTweet: data });
+  });
+});
+
+router.get("/findtweet/:author", (req, res) => {
+  User.findOne({ author: req.params.author }).then((data) => {
+    if (data) {
+      console.log("data is", data);
+      res.json({ result: true, findtweet: data.findtweet });
+    } else {
+      res.json({ result: false, error: "No tweets found" });
+    }
+  });
+});
+
+module.exports = router;
