33b1e23b9a63c4ca239de449762e5a83e984320a
Vincent S. <vincent.silvestri@hotmail.fr>
2025-02-14 17:28:29 +0100

    test

--- routes/tweet.js
+++ routes/tweet.js
@@ -91,7 +91,7 @@ router.get("/:hashtag", (req, res) => {
     });
 });
 
-router.delete("/dlttweets/", (req, res) => {
+router.delete("/dlttweets", (req, res) => {
   Tweet.deleteOne({ Tweet: Tweet }).then((data) => {
     res.send({ result: true, id: data });
   });
